0e9ef51be7af75707ca3dea37f7fc82776a442d5
Alena Shcheglova <a.shcheglova@student.tudelft.nl>
2021-10-28 13:42:58 +0300

    -

--- src/bounding_volume_hierarchy.cpp
+++ src/bounding_volume_hierarchy.cpp
@@ -235,7 +235,7 @@ glm::vec3 normalInterpolation(const auto& v0, const auto& v1, const auto& v2, Ra
     float u = (glm::length(glm::cross((p - v0.position), (v2.position - v0.position))) / 2.0f) / bigTriangleArea;
     float v = (glm::length(glm::cross((v0.position - v1.position), (p - v1.position))) / 2.0f) / bigTriangleArea;
 
-   // hitInfo.normal = glm::normalize(v0.normal * w + v1.normal * u + v2.normal * v);
+    // hitInfo.normal = glm::normalize(v0.normal * w + v1.normal * u + v2.normal * v);
     return glm::vec3{ w,u,v };
 
     /*
